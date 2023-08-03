8aaa6c59df6883d221bcf1badad40d465e40dceb
Zulin1@mail.ru <Neverhood1!>
2023-08-03 19:28:08 +0700

    - Сделал сериализацию словаря - И вывод его в инспектор, с редактированием полей и ключей, удалением и добавлением.

---
+++ Assets/Scripts/DictionaryInspect/DictionaryInspectDrawer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.DictionaryInspect
+{
+    [CustomPropertyDrawer(typeof(DictionaryInspectAttribute))]
+    public class DictionaryInspectDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty keysProperty = property.FindPropertyRelative("keys");
+            SerializedProperty valuesProperty = property.FindPropertyRelative("values");
+
+            if (keysProperty != null && keysProperty.isArray)
+            {
+
+                for (int i = 0; i < keysProperty.arraySize; i++)
+                {
+                    SerializedProperty keyProperty = keysProperty.GetArrayElementAtIndex(i);
+                    SerializedProperty valueProperty = valuesProperty.GetArrayElementAtIndex(i);
+
+                    EditorGUI.LabelField(new Rect(position.x, position.y, 50, EditorGUIUtility.singleLineHeight), "Key");
+                    EditorGUI.PropertyField(new Rect(position.x + 55, position.y, 50, EditorGUIUtility.singleLineHeight), keyProperty, GUIContent.none);
+
+                    EditorGUI.LabelField(new Rect(position.x + 110, position.y, 50, EditorGUIUtility.singleLineHeight), "Value");
+                    EditorGUI.PropertyField(new Rect(position.x + 165, position.y, 50, EditorGUIUtility.singleLineHeight), valueProperty, GUIContent.none);
+                    if (GUI.Button(new Rect(position.x + 220, position.y, 30, EditorGUIUtility.singleLineHeight), "X"))
+                    {
+                        keysProperty.DeleteArrayElementAtIndex(i);
+                        valuesProperty.DeleteArrayElementAtIndex(i);
+                    }
+
+                    position.y += EditorGUIUtility.singleLineHeight;
+                }
+                
+                if (GUI.Button(new Rect(position.x, position.y, EditorGUIUtility.currentViewWidth, EditorGUIUtility.singleLineHeight), "Add"))
+                {
+                    var index = keysProperty.arraySize;
+                    keysProperty.InsertArrayElementAtIndex(index);
+                    keysProperty.GetArrayElementAtIndex(index).stringValue = "";
+                    valuesProperty.InsertArrayElementAtIndex(index);
+                    valuesProperty.GetArrayElementAtIndex(index).stringValue = "";
+                }
+
+            }
+            else
+            {
+                Debug.Log("Not array!");
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty keysProperty = property.FindPropertyRelative("keys");
+            return EditorGUIUtility.singleLineHeight * (keysProperty.arraySize + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/DictionaryInspect/SerializableDictionary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.DictionaryInspect
+{
+
+    [System.Serializable]
+    public class SerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        private List<TKey> keys = new List<TKey>();
+        [SerializeField]
+        private List<TValue> values = new List<TValue>();
+
+        private Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+        // Method to add a key-value pair to the dictionary
+        public void Add(TKey key, TValue value)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                keys.Add(key);
+                values.Add(value);
+                dictionary.Add(key, value);
+            }
+        }
+
+        // Method to remove a key-value pair from the dictionary
+        public void Remove(TKey key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                int index = keys.IndexOf(key);
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+                dictionary.Remove(key);
+            }
+        }
+
+        // Method to get the value associated with a given key
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return dictionary.TryGetValue(key, out value);
+        }
+
+        // Method to implement ISerializationCallbackReceiver for custom serialization
+        public void OnBeforeSerialize()
+        {
+            keys.Clear();
+            values.Clear();
+
+            foreach (var pair in dictionary)
+            {
+                keys.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            dictionary.Clear();
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                if (!dictionary.ContainsKey(keys[i]))
+                {
+                    dictionary.Add(keys[i], values[i]);
+                }
+            }
+        }
+    }
+}
